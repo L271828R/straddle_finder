7fe938506ce066a1eb9ab8a76ac15b6c47cb05c1
BuildTools <luminai@gmail.com>
2019-08-11 00:23:11 -0400

    fixed major bugs added configure file now save on each ticker

---
+++ conf.py
@@ -0,0 +1,4 @@
+conf = { "random_time": True,
+        "headless": True,
+        "limit_dates": 5
+}
--- sf.py
+++ sf.py
@@ -1,12 +1,14 @@
 import selenium
+import sys
+import random
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from datetime import datetime
-import random
 from pymongo import MongoClient 
-import time
 from tickers import tickers
+from conf import conf
 
 
 class OptionTypes:
@@ -25,18 +27,17 @@ class OptionPage:
         self.dates = None
     
     def goto(self, ticker, date=None):
-        if self.once == False:
-            url = 'https://finance.yahoo.com/quote/__TICKER__/options?p=__TICKER__&straddle=true'
-            url = url.replace('__TICKER__', ticker)
-            self.driver.get(url)
-            self.driver.implicitly_wait(8)
-            self.once = True
+        url = 'https://finance.yahoo.com/quote/__TICKER__/options?p=__TICKER__&straddle=true'
+        url = url.replace('__TICKER__', ticker)
+        self.driver.get(url)
+        self.driver.implicitly_wait(8)
+        self.once = True
 
         if date is not None:
-            time.sleep(5)
+            time.sleep(8)
             el_sel = Select(self.driver.find_element_by_tag_name("select"))
             el_sel.select_by_visible_text(date.strftime("%B %-d, %Y"))
-        self.driver.implicitly_wait(3)
+        time.sleep(3)
         if self.conf['random_time'] == True:
             self.driver.implicitly_wait(random.randrange(5, 15))
         else:
@@ -90,11 +91,10 @@ def get_straddles(driver ,ticker, conf):
     op = OptionPage(driver, conf)
     arr = []
     isFirst = True
-    if op.dates == None:
-        if conf["limit_dates"] is not None:
-            op.dates = op.get_available_months()[:conf["limit_dates"]]
-        else:
-            op.dates = op.get_available_months()
+    if conf["limit_dates"] is not None:
+        op.dates = op.get_available_months()[:conf["limit_dates"]]
+    else:
+        op.dates = op.get_available_months()
     for date in op.dates:
         if isFirst == False:
             op.goto(ticker, date)
@@ -109,65 +109,81 @@ def driver_cleanup(driver):
     driver.close()
     driver.quit()
 
-def main():
-    print('Straddle Finder') 
-    conf = { "random_time": True,
-            "headless": True,
-            "limit_dates": 5
+
+def isSaved(conn, ticker):
+    db = conn.database 
+    collection = db.straddles
+    now = datetime.now().strftime("%Y-%m-%d")
+    param = {
+        'ticker': ticker,
+        'time_stamp': now
     }
+    cursor = collection.find(param)
+    if cursor.count() > 0:
+        # print("cursor returned true for {} {}".format(ticker, now))
+        return True
+    else:
+        # print("cursor returned false for {} {}".format(ticker, now))
+        return False
+
+def create_driver(conf):
     chrome_options = Options()  
     if conf["headless"]:
         chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(".//chromedriver", chrome_options=chrome_options)
+    return  webdriver.Chrome(".//chromedriver", chrome_options=chrome_options)
+    
+
+def main(conn):
+    print('Straddle Finder') 
+    driver = create_driver(conf)
     # tickers = ['aapl','spy']
     straddles = []
     op = OptionPage(driver=driver, conf=conf)
     for count, tick in enumerate(tickers):
-        print("{} of {}, currently {}".format(str(count + 1), str(len(tickers)), tick))
-        op.goto(ticker=tick, date=None)
-        try:
-            arr = get_straddles(driver=driver, ticker=tick, conf=conf)
-            straddles = straddles + arr
-            op.dates = None
-        except:
-            pass
+        if not isSaved(conn=conn, ticker=tick):
+            print("{} of {}, currently {}".format(str(count + 1), str(len(tickers)), tick))
+            op.goto(ticker=tick, date=None)
+            try:
+                arr = get_straddles(driver=driver, ticker=tick, conf=conf)
+                for straddle in arr:
+                    save_straddle(conn, straddle)
+                op.dates = None
+            except Exception as e:
+                print("Unexpected error:", sys.exc_info()[0])
+                print(e)
 
     driver_cleanup(driver)
+    print('returning straddles')
     return straddles
+
+
+def save_straddle(conn, straddle):
+    db = conn.database 
+    collection = db.straddles
+    if straddle is not None:
+        cursor = collection.find({
+            'strike':straddle['strike'],
+            'date': straddle['date'],
+            'type': straddle['type'],
+            'ticker': straddle['ticker'],
+            'days_to_expiration': straddle['days_to_expiration']
+        })
+
+        if cursor.count() == 0:
+            print("inserting....")
+            collection.insert_one(straddle)
+        else:
+            print("not inserting...")
+            pass
 
 if __name__ == '__main__':
     conn = None
-
     try: 
        conn = MongoClient() 
        print("Connected successfully!!!") 
     except:   
        print("Could not connect to MongoDB") 
-
-    db = conn.database 
-    collection = db.straddles 
-    straddles = main()
-    for straddle in straddles:
-        if straddle is not None:
-            cursor = collection.find({
-                'strike':straddle['strike'],
-                'date': straddle['date'],
-                'type': straddle['type'],
-                'ticker': straddle['ticker'],
-                'days_to_expiration': straddle['days_to_expiration']
-            })
-
-            if cursor.count() == 0:
-                collection.insert_one(straddle)
-            else:
-                pass
-    print('done')
-
-    # collection.insert_one(emp_rec1) 
-
-    # cursor = collection.find({}) 
-    # for record in cursor: 
-    #     print(record) 
+    main(conn=conn)
 
 
 
